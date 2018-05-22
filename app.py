b5c9d06a88c5657cf168ec6dc10866a85e4096bc
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2018-05-21 21:51:21 -0500

    Add temporary in-memory list of pokemon, and add endpoints for returning a list of all pokemon and all pokemon by generation

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({'Message': 'Hello, World!'})
+pokemon = [{"pokemon_id": 1,
+    "name": "Bulbasaur",
+    "gen": 1,
+    "rank": 1,
+    "genRank": 1},{"pokemon_id": 2,
+    "name": "Ivysaur",
+    "gen": 1,
+    "rank": 2,
+    "genRank": 2}]
 
-@app.route('/hello')
-def hello():
-    return jsonify({'Message': 'Hello, World!'})
+@app.route('/pokeranker/api/v1.0/pokemon', methods=['GET'])
+def get_pokemon_list():
+    return jsonify(pokemon)
+
+@app.route('/pokeranker/api/v1.0/pokemon/gen/<int:gen>', methods=['GET'])
+def get_pokemon_list_gen(gen):
+    if type(gen) is not int or gen < 1 or gen > 7:
+        abort(404)
+    gen_pokemon = [poke for poke in pokemon if poke["gen"] == gen]
+    return jsonify(gen_pokemon)
 
 if __name__ == '__main__':
     app.run(debug=False)
