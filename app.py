fea399ed68d8d4624483d6cb178987cd05ec6bc4
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2018-05-24 18:30:12 -0500

    Move from in memory storage to using the db

--- app.py
+++ app.py
@@ -1,50 +1,20 @@
 from flask import Flask, jsonify, abort, make_response, request
 from random import randint
 from tasks import update_elos
+from pymongo import MongoClient, DESCENDING, ASCENDING
+
+client = MongoClient()
+db = client.pokedatabase
+pokemon_collection = db.pokemon_collection
+vote_collection = db.vote_collection
 
 app = Flask(__name__)
-
-# TODO this data will eventually be stored in a database
-pokemon = [
-    {
-        "pokemon_id": 1,
-        "name": "Bulbasaur",
-        "gen": 1,
-        "rank": 1,
-        "genRank": 1,
-        "elo": 1200.0
-    },
-    {
-        "pokemon_id": 2,
-        "name": "Ivysaur",
-        "gen": 1,
-        "rank": 2,
-        "genRank": 2,
-        "elo": 1200.0
-    },
-    {
-        "pokemon_id": 152,
-        "name": "Chikorita",
-        "gen": 2,
-        "rank": 3,
-        "genRank": 1,
-        "elo": 1200.0
-    },
-    {
-        "pokemon_id": 153,
-        "name": "Bayleef",
-        "gen": 2,
-        "rank": 4,
-        "genRank": 2,
-        "elo": 1200
-    }]
-
-votes = []
 
 # GET /pokemon
 @app.route('/pokeranker/api/v1.0/pokemon', methods=['GET'])
 def get_pokemon_list():
-    # TODO return this as a list sorted by ELO
+    pokemon_cursor = pokemon_collection.find({}, {'_id': False}).sort("elo", DESCENDING)
+    pokemon = [poke for poke in pokemon_cursor]
     return jsonify(pokemon)
 
 # GET /pokemon/gen/{gen}
@@ -53,8 +23,8 @@ def get_pokemon_list_gen(gen):
     # TODO write helper to validate the gen
     if type(gen) is not int or gen < 1 or gen > 7:
         abort(404)
-    gen_pokemon = [poke for poke in pokemon if poke["gen"] == gen]
-    # TODO return this as a list sorted by ELO
+    pokemon_cursor = pokemon_collection.find({'gen':gen}, {'_id': False}).sort("elo", DESCENDING)
+    gen_pokemon = [poke for poke in pokemon_cursor]
     return jsonify(gen_pokemon)
 
 # GET /pokemon/{pokemon_id} (int)
@@ -63,16 +33,16 @@ def get_pokemon(pokemon_id):
     # TODO write helper to validate the ids
     if type(pokemon_id) is not int or pokemon_id < 1 or pokemon_id > 807:
         abort(404)
-    found = False
-    for poke in pokemon:
-        if poke["pokemon_id"] == pokemon_id:
-            return jsonify(poke)
-    if not found:
+    pokemon = pokemon_collection.find_one({'pokemon_id':pokemon_id}, {'_id': False})
+    if pokemon is None:
         abort(404)
+    return jsonify(pokemon)
 
 # GET /votes
 @app.route('/pokeranker/api/v1.0/votes', methods=['GET'])
 def get_votes():
+    votes_cursor = vote_collection.find({}, {'_id': False})
+    votes = [poke for poke in votes_cursor]
     return jsonify(votes)
 
 # POST /votes
@@ -89,23 +59,17 @@ def submit_vote():
         abort(404)
     if type(loser_id) is not int or loser_id < 1 or loser_id > 807:
         abort(404)
-    vote_id = -1
-    if len(votes) == 0:
-        vote_id = 1
-    else:
-        vote_id = votes[-1]['vote_id'] + 1
-    if vote_id == -1:
-        abort(400)
+
+    vote_id = vote_collection.count() + 1
     vote = {
         'vote_id': vote_id,
         "status": "pending",
         "winner_id": winner_id,
         "loser_id": loser_id
     }
-    votes.append(vote)
-
+    vote_collection.insert_one(vote)
     result = update_elos.delay(winner_id, loser_id, vote_id)
-
+    vote = vote_collection.find_one({'vote_id':vote_id}, {'_id': False})
     return jsonify(vote), 201
 
 
@@ -113,28 +77,29 @@ def submit_vote():
 @app.route('/pokeranker/api/v1.0/votes/<int:vote_id>', methods=['GET'])
 def get_vote_status(vote_id):
     # TODO write helper to validate the ids
-    if type(vote_id) is not int or vote_id < 0 or vote_id > len(votes):
+    if type(vote_id) is not int or vote_id <= 0 or vote_id > vote_collection.count():
         abort(404)
-    found = False
-    for vote in votes:
-        if vote["vote_id"] == vote_id:
-            return jsonify(vote)
+    vote = vote_collection.find_one({'vote_id':vote_id}, {'_id': False})
+    return jsonify(vote)
     if not found:
         abort(404)
 
 # GET /match
 @app.route('/pokeranker/api/v1.0/match', methods=['GET'])
 def get_match():
-    index_one = randint(0,len(pokemon)-1)
-    index_two = randint(0,len(pokemon)-1)
+    # TODO get len from db
+    index_one = randint(0, pokemon_collection.count()-1)
+    index_two = randint(0, pokemon_collection.count()-1)
     while index_one == index_two:
-        index_two = randint(0,len(pokemon)-1)
+        index_two = randint(0, pokemon_collection.count()-1)
 
+    pokemon_one = pokemon_collection.find({}, {'_id': False})[index_one]
+    pokemon_two = pokemon_collection.find({}, {'_id': False})[index_two]
     match = {
-        "one": pokemon[index_one]["name"],
-        "two": pokemon[index_two]["name"],
-        "oneid": pokemon[index_one]["pokemon_id"],
-        "twoid": pokemon[index_two]["pokemon_id"]
+        "one": pokemon_one["name"],
+        "two": pokemon_two["name"],
+        "oneid": pokemon_one["pokemon_id"],
+        "twoid": pokemon_two["pokemon_id"]
     }
     return jsonify(match)
 
@@ -144,18 +109,21 @@ def get_match_gen(gen):
     # TODO write helper to validate the gen
     if type(gen) is not int or gen < 1 or gen > 7:
         abort(404)
-    gen_pokemon = [poke for poke in pokemon if poke["gen"] == gen]
+    pokemon_cursor = pokemon_collection.find({'gen':gen}, {'_id': False})
+    gen_pokemon = [poke for poke in pokemon_cursor]
 
     index_one = randint(0,len(gen_pokemon)-1)
     index_two = randint(0,len(gen_pokemon)-1)
     while index_one == index_two:
         index_two = randint(0,len(gen_pokemon)-1)
 
+    pokemon_one = gen_pokemon[index_one]
+    pokemon_two = gen_pokemon[index_two]
     match = {
-        "one": gen_pokemon[index_one]["name"],
-        "two": gen_pokemon[index_two]["name"],
-        "oneid": gen_pokemon[index_one]["pokemon_id"],
-        "twoid": gen_pokemon[index_two]["pokemon_id"]
+        "one": pokemon_one["name"],
+        "two": pokemon_two["name"],
+        "oneid": pokemon_one["pokemon_id"],
+        "twoid": pokemon_two["pokemon_id"]
     }
     return jsonify(match)
 
